b5844722efcea5febd82f5b24cc875c7424016dd
Dima Kunin <mr.kunin@gmail.com>
2017-04-19 10:46:50 +0200

    Add nicer output.

--- hw2/src/main/java/ru/otus/kunin/memory/SizeOf.java
+++ hw2/src/main/java/ru/otus/kunin/memory/SizeOf.java
@@ -1,17 +1,31 @@
 package ru.otus.kunin.memory;
 
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 public class SizeOf {
 
   public static <T> long measure(Supplier<T> objectCreator) {
-    return measure(objectCreator, String.valueOf(objectCreator.get()));
+    final T obj = objectCreator.get();
+    final String stringValue;
+    // Not all types are here, but the ones we need
+    if (obj instanceof Object[]) {
+      stringValue = Arrays.toString((Object[]) obj);
+    } else if (obj instanceof int[]) {
+      stringValue = Arrays.toString((int[]) obj);
+    } else if (obj instanceof boolean[]) {
+      stringValue = Arrays.toString((boolean[]) obj);
+    } else {
+      stringValue = obj.toString();
+    }
+    return measure(objectCreator, obj.getClass().getSimpleName() + " " + stringValue);
   }
 
 
   public static <T> long measure(Supplier<T> objectCreator, String tag) {
     final long almostTakesThatMuchMemory = getAlmostTakesThatMuchMemory(objectCreator, tag);
     // Calling GC outside of a method seems to improve accuracy
+    // maybe it is due scope changing
     SynchronousGC.collect();
     return almostTakesThatMuchMemory;
   }
@@ -30,7 +44,7 @@ public class SizeOf {
     SynchronousGC.collect();
     final long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
     final long almostTakesThatMuchMemory = Math.round((memoryAfter - memoryBefore) / (double) objects.length);
-    System.out.printf("SizeOf [%s] is [%d]\n", tag, almostTakesThatMuchMemory);
+    System.out.printf("[%d] is SizeOf %s \n", almostTakesThatMuchMemory, tag);
     return almostTakesThatMuchMemory;
   }
 
