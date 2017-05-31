eedd69b116340d082c3ee42ff4a81698866e3b22
Dima Kunin <mr.kunin@gmail.com>
2017-05-31 15:45:44 +0200

    Set up things for objects.

--- hw7/src/main/java/Main.java
+++ hw7/src/main/java/Main.java
@@ -1,6 +1,48 @@
+import com.google.common.collect.Lists;
+import java.util.List;
 import ru.otus.kunin.DSON;
 
 public class Main {
+
+  private static class Cat {
+
+    public final String name;
+    public final int age;
+    public final List<Cat> friends;
+
+    private Cat(final String name, final int age, final List<Cat> friends) {
+      this.name = name;
+      this.age = age;
+      this.friends = friends;
+    }
+
+    static class Builder {
+
+      private String name;
+      private int age;
+      private List<Cat> friends;
+
+      Builder setName(String name) {
+        this.name = name;
+        return this;
+      }
+
+      Builder setAge(int age) {
+        this.age = age;
+        return this;
+      }
+
+      Builder setFriends(List<Cat> friends) {
+        this.friends = friends;
+        return this;
+      }
+
+      Cat build() {
+        return new Cat(name, age, friends);
+      }
+
+    }
+  }
 
   public static void main(String[] args) {
     System.out.println("<json>");
@@ -10,6 +52,13 @@ public class Main {
     System.out.println(DSON.toJsonObject(1).toString());
     System.out.println(DSON.toJsonObject(1.1).toString());
     System.out.println(DSON.toJsonObject(false).toString());
+
+    final Cat aLonelyCat = new Cat.Builder()
+        .setName("Fluffy")
+        .setAge(3)
+        .setFriends(Lists.newArrayList())
+        .build();
+    System.out.println(DSON.toJsonObject(aLonelyCat));
   }
 
 }
