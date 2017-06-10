1c7387867d81e0e58dfd0e730134ef82886f9058
Dima Kunin <mr.kunin@gmail.com>
2017-06-10 22:02:30 +0200

    print jdbc version

--- hw_8_dorm/src/main/java/ru/otus/kunin/dorm/main/Main.java
+++ hw_8_dorm/src/main/java/ru/otus/kunin/dorm/main/Main.java
@@ -3,14 +3,19 @@ package ru.otus.kunin.dorm.main;
 import ru.otus.kunin.dorm.Connector;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
 public class Main {
 
   public static void main(String[] args) throws SQLException {
+    // NOTE: There is not need to register driver since jdbc 4.0
+    // see http://docs.oracle.com/javase/tutorial/jdbc/basics/connecting.html
     System.out.println("<dorm>");
     try (Connection connection = Connector.connect()) {
-      System.out.println("Connected to: " + connection.getMetaData().getURL());
+      final DatabaseMetaData metaData = connection.getMetaData();
+      System.out.println("Connected to: " + metaData.getURL() + "\n" +
+          "jdbc version: " + metaData.getJDBCMajorVersion() + "." + metaData.getJDBCMinorVersion());
     }
   }
 
