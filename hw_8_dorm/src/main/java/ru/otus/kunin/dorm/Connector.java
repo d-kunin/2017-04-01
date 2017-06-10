c6e480efeceaea3576976dcbe7f76354e63885e4
Dima Kunin <mr.kunin@gmail.com>
2017-06-10 22:22:42 +0200

    Use MysqlDataSource

--- hw_8_dorm/src/main/java/ru/otus/kunin/dorm/Connector.java
+++ hw_8_dorm/src/main/java/ru/otus/kunin/dorm/Connector.java
@@ -1,5 +1,8 @@
 package ru.otus.kunin.dorm;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,12 +10,28 @@ import java.util.Properties;
 
 public class Connector {
 
-  public static Connection connect() throws SQLException {
+  private static final String USER = "tully";
+  private static final String PASSWORD = "tully";
+  private static final int PORT = 3306;
+  private static final String SERVER_NAME = "localhost";
+  private static final String DB_NAME = "db_example";
+
+  public static Connection connectWithDriverManager() throws SQLException {
     Properties connectionProperties = new Properties();
-    connectionProperties.put("user", "tully");
-    connectionProperties.put("password", "tully");
+    connectionProperties.put("user", USER);
+    connectionProperties.put("password", PASSWORD);
     connectionProperties.put("useSSL", false);
-    return DriverManager.getConnection("jdbc:mysql://localhost:3306/db_example", connectionProperties);
+    return DriverManager.getConnection("jdbc:mysql://localhost:"+ PORT + "/" + DB_NAME, connectionProperties);
+  }
+
+  public static DataSource createDataSource() {
+    final MysqlDataSource mysqlDataSource = new MysqlDataSource();
+    mysqlDataSource.setUser(USER);
+    mysqlDataSource.setPassword(PASSWORD);
+    mysqlDataSource.setPort(PORT);
+    mysqlDataSource.setServerName(SERVER_NAME);
+    mysqlDataSource.setDatabaseName(DB_NAME);
+    return mysqlDataSource;
   }
 
 }
--- hw_8_dorm/src/main/java/ru/otus/kunin/dorm/main/Main.java
+++ hw_8_dorm/src/main/java/ru/otus/kunin/dorm/main/Main.java
@@ -12,7 +12,7 @@ public class Main {
     // NOTE: There is not need to register driver since jdbc 4.0
     // see http://docs.oracle.com/javase/tutorial/jdbc/basics/connecting.html
     System.out.println("<dorm>");
-    try (Connection connection = Connector.connect()) {
+    try (Connection connection = Connector.createDataSource().getConnection()) {
       final DatabaseMetaData metaData = connection.getMetaData();
       System.out.println("Connected to: " + metaData.getURL() + "\n" +
           "jdbc version: " + metaData.getJDBCMajorVersion() + "." + metaData.getJDBCMinorVersion());
