42cb5cfe011984c73bf88e1554d8a9c67384f629
Dima Kunin <mr.kunin@gmail.com>
2017-06-16 18:41:26 +0200

    Add tests for parameters list.

--- hw_8_dorm/src/test/java/ru/otus/kunin/dorm/SqlGeneratorImplTest.java
+++ hw_8_dorm/src/test/java/ru/otus/kunin/dorm/SqlGeneratorImplTest.java
@@ -16,6 +16,7 @@ public class SqlGeneratorImplTest {
     assertEquals(
         "create table dima_users (id bigint auto_increment primary key, name varchar(255), age int(3) not null default 0, displayName text);",
         createTable.getQuery());
+    assertEquals(0, createTable.getParameterList().size());
   }
 
   @Test
@@ -24,6 +25,7 @@ public class SqlGeneratorImplTest {
     assertEquals(
         "drop table dima_users;",
         dropTable.getQuery());
+    assertEquals(0, dropTable.getParameterList().size());
   }
 
   @Test
@@ -32,6 +34,10 @@ public class SqlGeneratorImplTest {
     assertEquals(
         "insert into dima_users (name, age, displayName) values (?, ?, ?);",
         insert.getQuery());
+    assertEquals(3, insert.getParameterList().size());
+    assertEquals("name", insert.getParameterList().get(1).getSqlName());
+    assertEquals("age", insert.getParameterList().get(2).getSqlName());
+    assertEquals("displayName", insert.getParameterList().get(3).getSqlName());
   }
 
   @Test
@@ -40,6 +46,11 @@ public class SqlGeneratorImplTest {
     assertEquals(
         "update dima_users set name=?, age=?, displayName=? where id=?;",
         update.getQuery());
+    assertEquals(4, update.getParameterList().size());
+    assertEquals("name", update.getParameterList().get(1).getSqlName());
+    assertEquals("age", update.getParameterList().get(2).getSqlName());
+    assertEquals("displayName", update.getParameterList().get(3).getSqlName());
+    assertEquals(typeMapping.getIdField(), update.getParameterList().get(4));
   }
 
   @Test
@@ -49,6 +60,8 @@ public class SqlGeneratorImplTest {
         "select * from dima_users where id=?;",
         selectOne.getQuery()
     );
+    assertEquals(1, selectOne.getParameterList().size());
+    assertEquals(typeMapping.getIdField(), selectOne.getParameterList().get(1));
   }
 
   @Test
@@ -58,6 +71,7 @@ public class SqlGeneratorImplTest {
         "select * from dima_users;",
         selectAll.getQuery()
     );
+    assertEquals(0, selectAll.getParameterList().size());
   }
 
 }
