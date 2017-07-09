051ec09ad1a4a4bc2402edde7b463f57a65b8cac
Dima Kunin <mr.kunin@gmail.com>
2017-07-09 17:52:39 +0200

    closed!

--- dcache/src/main/java/ru/otus/kunin/dcache/impl/DcacheImpl.java
+++ dcache/src/main/java/ru/otus/kunin/dcache/impl/DcacheImpl.java
@@ -3,8 +3,16 @@ package ru.otus.kunin.dcache.impl;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Maps;
+
 import ru.otus.kunin.dcache.Dcache;
 
+import java.lang.ref.SoftReference;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.cache.CacheManager;
 import javax.cache.configuration.CacheEntryListenerConfiguration;
 import javax.cache.configuration.Configuration;
@@ -13,12 +21,6 @@ import javax.cache.integration.CompletionListener;
 import javax.cache.processor.EntryProcessor;
 import javax.cache.processor.EntryProcessorException;
 import javax.cache.processor.EntryProcessorResult;
-import java.lang.ref.SoftReference;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.ConcurrentMap;
 
 import static java.util.stream.Collectors.toMap;
 
@@ -27,6 +29,7 @@ public class DcacheImpl<K, V> implements Dcache<K, V> {
   private final ConcurrentMap<K, DcacheEntry<K, V>> map = Maps.newConcurrentMap();
   private final Optional<CacheLoader<K, V>> cacheLoader;
   private final Optional<CacheLoader<K, V>> cacheWriter;
+  private final AtomicBoolean isClosed = new AtomicBoolean(false);
 
   public DcacheImpl() {
     this(Optional.empty(), Optional.empty());
@@ -40,6 +43,7 @@ public class DcacheImpl<K, V> implements Dcache<K, V> {
 
   @Override
   public V get(final K key) {
+    throwIfClosed();
     final Optional<DcacheEntry<K, V>> entry = Optional.ofNullable(map.get(validateKey(key)));
     // TODO load if not present
     return entry.map(DcacheEntry::getValue).orElse(null);
@@ -47,6 +51,7 @@ public class DcacheImpl<K, V> implements Dcache<K, V> {
 
   @Override
   public Map<K, V> getAll(final Set<? extends K> set) {
+    throwIfClosed();
     return set.stream()
         .map(map::get)
         .filter(ref -> ref != null && ref.get() != null)
@@ -58,6 +63,7 @@ public class DcacheImpl<K, V> implements Dcache<K, V> {
 
   @Override
   public boolean containsKey(final K key) {
+    throwIfClosed();
     return Optional.ofNullable(map.get(validateKey(key))).map(SoftReference::get).isPresent();
   }
 
@@ -65,18 +71,21 @@ public class DcacheImpl<K, V> implements Dcache<K, V> {
   public void loadAll(final Set<? extends K> keys,
                       final boolean reload,
                       final CompletionListener completionListener) {
+    throwIfClosed();
     // TODO load all
     // TODO notify
   }
 
   @Override
   public void put(final K k, final V v) {
+    throwIfClosed();
     map.put(k, createEntry(k, v));
     // TODO notify
   }
 
   @Override
   public V getAndPut(final K k, final V v) {
+    throwIfClosed();
     final Optional<DcacheEntry<K, V>> oldEntry = Optional.of(map.replace(k, createEntry(k, v)));
     // TODO notify
     return oldEntry.map(DcacheEntry::getValue).orElse(null);
@@ -84,11 +93,13 @@ public class DcacheImpl<K, V> implements Dcache<K, V> {
 
   @Override
   public void putAll(final Map<? extends K, ? extends V> map) {
+    throwIfClosed();
     map.forEach(this::put);
   }
 
   @Override
   public boolean putIfAbsent(final K k, final V v) {
+    throwIfClosed();
     final DcacheEntry<K, V> oldEntry = map.putIfAbsent(k, createEntry(k, v));
     return Optional.ofNullable(oldEntry).map(DcacheEntry::getValue).isPresent();
     // TODO notify
@@ -96,50 +107,66 @@ public class DcacheImpl<K, V> implements Dcache<K, V> {
 
   @Override
   public boolean remove(final K k) {
+    throwIfClosed();
     return Optional.ofNullable(map.remove(k)).map(DcacheEntry::getValue).isPresent();
   }
 
   @Override
   public boolean remove(final K k, final V v) {
+    throwIfClosed();
     return map.remove(k, createEntry(k, v));
   }
 
   @Override
   public V getAndRemove(final K k) {
+    throwIfClosed();
     return Optional.ofNullable(map.remove(k)).map(DcacheEntry::getValue).orElse(null);
     // TODO notify
   }
 
   @Override
   public boolean replace(final K k, final V oldValue, final V newValue) {
+    throwIfClosed();
     return map.replace(k, createEntry(k, oldValue), createEntry(k, newValue));
     // TODO notify
   }
 
   @Override
-  public boolean replace(final K k, final V v) {
-    return false;
+  public boolean replace(final K key, final V newValue) {
+    throwIfClosed();
+    return Optional.ofNullable(map.replace(key, createEntry(key, newValue))).isPresent();
     // TODO notify
   }
 
   @Override
   public V getAndReplace(final K k, final V v) {
-    return null;
+    throwIfClosed();
+    return Optional.ofNullable(map.replace(k, createEntry(k, v)))
+        .map(DcacheEntry::getValue)
+        .orElse(null);
     // TODO notify
   }
 
   @Override
   public void removeAll(final Set<? extends K> set) {
-    // TODO notify
+    throwIfClosed();
+    if (set.isEmpty()) {
+      return;
+    }
+    set.stream().forEach(this::remove);
   }
 
   @Override
   public void removeAll() {
-    // TODO notify
+    throwIfClosed();
+    Set<K> keys = map.keySet();
+    keys.stream().forEach(this::remove);
   }
 
   @Override
   public void clear() {
+    throwIfClosed();
+    map.clear();
   }
 
   @Override
@@ -151,6 +178,7 @@ public class DcacheImpl<K, V> implements Dcache<K, V> {
   public <T> T invoke(final K k,
                       final EntryProcessor<K, V, T> entryProcessor,
                       final Object... objects) throws EntryProcessorException {
+    throwIfClosed();
     return null;
   }
 
@@ -158,6 +186,7 @@ public class DcacheImpl<K, V> implements Dcache<K, V> {
   public <T> Map<K, EntryProcessorResult<T>> invokeAll(final Set<? extends K> set,
                                                        final EntryProcessor<K, V, T> entryProcessor,
                                                        final Object... objects) {
+    throwIfClosed();
     return null;
   }
 
@@ -173,18 +202,21 @@ public class DcacheImpl<K, V> implements Dcache<K, V> {
 
   @Override
   public void close() {
-
+    throwIfClosed();
+    isClosed.set(true);
   }
 
   @Override
   public boolean isClosed() {
-    return false;
+    return isClosed.get();
   }
 
   @Override
   public <T> T unwrap(final Class<T> aClass) {
-    // TODO return self if the same class?
-    return null;
+    if (DcacheImpl.class == aClass || Dcache.class == aClass) {
+      return (T) this;
+    }
+    throw new IllegalArgumentException();
   }
 
   @Override
@@ -200,6 +232,12 @@ public class DcacheImpl<K, V> implements Dcache<K, V> {
   @Override
   public Iterator<Entry<K, V>> iterator() {
     return null;
+  }
+
+  private void throwIfClosed() {
+    if (isClosed.get()) {
+      throw new IllegalStateException("The cache is closed");
+    }
   }
 
   private K validateKey(K key) {
