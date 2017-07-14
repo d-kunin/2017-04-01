27155818977ea9faace15c155efe9452d108a1fb
Dima Kunin <mr.kunin@gmail.com>
2017-07-14 21:07:26 +0200

    Tests skeleton

---
+++ dcache/src/test/java/ru/otus/kunin/dicache/base/DiCacheTest.java
@@ -0,0 +1,102 @@
+package ru.otus.kunin.dicache.base;
+
+import org.junit.Test;
+
+public class DiCacheTest {
+  @Test
+  public void get() throws Exception {
+  }
+
+  @Test
+  public void getAll() throws Exception {
+  }
+
+  @Test
+  public void containsKey() throws Exception {
+  }
+
+  @Test
+  public void put() throws Exception {
+  }
+
+  @Test
+  public void getAndPut() throws Exception {
+  }
+
+  @Test
+  public void putAll() throws Exception {
+  }
+
+  @Test
+  public void putIfAbsent() throws Exception {
+  }
+
+  @Test
+  public void remove() throws Exception {
+  }
+
+  @Test
+  public void remove1() throws Exception {
+  }
+
+  @Test
+  public void getAndRemove() throws Exception {
+  }
+
+  @Test
+  public void replace() throws Exception {
+  }
+
+  @Test
+  public void replace1() throws Exception {
+  }
+
+  @Test
+  public void getAndReplace() throws Exception {
+  }
+
+  @Test
+  public void removeAll() throws Exception {
+  }
+
+  @Test
+  public void removeAll1() throws Exception {
+  }
+
+  @Test
+  public void clear() throws Exception {
+  }
+
+  @Test
+  public void invoke() throws Exception {
+  }
+
+  @Test
+  public void invokeAll() throws Exception {
+  }
+
+  @Test
+  public void close() throws Exception {
+  }
+
+  @Test
+  public void isClosed() throws Exception {
+  }
+
+  @Test
+  public void unwrap() throws Exception {
+  }
+
+  @Test
+  public void registerCacheEntryListener() throws Exception {
+  }
+
+  @Test
+  public void deregisterCacheEntryListener() throws Exception {
+  }
+
+  @Test
+  public void registerCacheEntryListener1() throws Exception {
+  }
+
+}
