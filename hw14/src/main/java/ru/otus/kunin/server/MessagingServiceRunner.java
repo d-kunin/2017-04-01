e7e282e83a69d1412506f0c40ed510ea65ec6c71
Dima Kunin <mr.kunin@gmail.com>
2017-08-20 23:30:47 +0200

    Serve static

--- hw14/src/main/java/ru/otus/kunin/server/MessagingServiceRunner.java
+++ hw14/src/main/java/ru/otus/kunin/server/MessagingServiceRunner.java
@@ -1,20 +1,32 @@
 package ru.otus.kunin.server;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
 import ru.otus.kunin.dicache.DiCache;
 import ru.otus.kunin.dorm.base.DormImpl;
 
-public class ServerRunner {
+public class MessagingServiceRunner {
 
   public static void main(String[] args) throws Exception {
     final Server server = new Server(8090);
     final ServletHandler servletHandler = new ServletHandler();
-    server.setHandler(servletHandler);
 
     final CacheServlet cacheServlet = new CacheServlet(new DiCache<>(), DormImpl.create());
     servletHandler.addServletWithMapping(new ServletHolder(cacheServlet), "/cache");
+
+    final ResourceHandler resourceHandler = new ResourceHandler();
+    resourceHandler.setDirectoriesListed(true);
+    resourceHandler.setBaseResource(Resource.newClassPathResource("./static/"));
+
+    server.setHandler(
+        new HandlerList(resourceHandler,
+                        servletHandler,
+                        new DefaultHandler()));
 
     server.start();
     server.join();
---
+++ hw14/src/main/resources/static/cache.html
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Welcome</title>
+</head>
+<body>
+<!-- send new value here -->
+<br/>
+<form action="cache" method="post">
+    <input type="hidden" name="action" value="write"/>
+    New key: <input type="text" name="new_key"><br/>
+    New value: <input type="text" name="new_value"><br/>
+    <input type="submit" value="Send">
+</form>
+<br/>
+<!-- read value here -->
+<form action="cache" method="get">
+    <input type="hidden" name="action" value="read"/>
+    Read by key: <input type="text" name="read_key" value="${read_key!}"><br>
+    Value is: <input type="text" value="${result_read!}" readonly><br>
+    <input type="submit" value="Send">
+</form>
+<br/>
+<!-- show stats here -->
+<br/>
+<table>
+    <#list stats as stat>
+        <tr>
+            <td>${stat.key}</td>
+            <td>${stat.value}</td>
+        </tr>
+    </#list>
+</table>
+<br/>
+<!-- operations log here -->
+<textarea readonly cols="80" rows="20">
+<#list logs as log>
+    ${log}
+</#list>
+</textarea>
+</body>
+</html>
