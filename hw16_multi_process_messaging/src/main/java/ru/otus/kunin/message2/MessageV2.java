97823c2b4d818f75534c4a4d6544b9c2631815c5
Dima Kunin <mr.kunin@gmail.com>
2017-09-08 18:01:35 +0200

    Defined MessageV2

--- hw16_multi_process_messaging/src/main/java/ru/otus/kunin/message2/MessageV2.java
+++ hw16_multi_process_messaging/src/main/java/ru/otus/kunin/message2/MessageV2.java
@@ -1,50 +1,66 @@
 package ru.otus.kunin.message2;
 
+import com.google.auto.value.AutoValue;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.kundzi.socket.channels.message.lvmessage.LvMessage;
 import ru.otus.kunin.messageSystem.Address;
 
 import javax.annotation.Nullable;
 
-public interface MessageV2 extends LvMessage {
+@AutoValue
+public abstract class MessageV2 implements LvMessage {
 
-  // Constants
-  @JsonIgnore
-  ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  @JsonCreator
+  public static MessageV2 create(
+      @JsonProperty("id") String id,
+      @JsonProperty("in_response_to") String inResponseTo,
+      @JsonProperty("type") String type,
+      @JsonProperty("from") Address from,
+      @JsonProperty("to") Address to,
+      @JsonProperty("payload") JsonNode payload
+  ) {
+    return new ru.otus.kunin.message2.AutoValue_MessageV2(id, inResponseTo, type, from, to, payload);
+  }
 
-  @JsonIgnore
-  String VERSION = "v1";
+  static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+  private static final String VERSION = "v1";
 
   @JsonProperty("id")
-  String id();
+  public abstract String id();
 
-  // Properties
   @Nullable
-  @JsonProperty("in_response_to_id")
-  String inResponseToId();
+  @JsonProperty("in_response_to")
+  public abstract String inResponseTo();
 
   @JsonProperty("type")
-  String type();
+  public abstract String type();
 
   @JsonProperty("from")
-  Address from();
+  public abstract Address from();
 
   @Nullable
   @JsonProperty("to")
-  Address to();
+  public abstract Address to();
 
-  // Default methods
+  @Nullable
+  @JsonProperty("payload")
+  public abstract JsonNode payload();
+
   @JsonProperty("version")
-  default String version() {
+  public String version() {
     return VERSION;
   }
 
   @JsonIgnore
   @Override
-  default byte[] data() {
+  public byte[] data() {
     try {
       return OBJECT_MAPPER.writeValueAsBytes(this);
     } catch (JsonProcessingException e) {
@@ -54,9 +70,9 @@ public interface MessageV2 extends LvMessage {
 
   @JsonIgnore
   @Override
-  default int length() {
-    // TODO not effective at all, please fix
-    // Though in practice length and data are called once, so it will not be as bad.
+  public int length() {
+    // This is not the most effective.
+    // But since fields are not guaranteed to be immutable it is a safer option.
     return data().length;
   }
 }
